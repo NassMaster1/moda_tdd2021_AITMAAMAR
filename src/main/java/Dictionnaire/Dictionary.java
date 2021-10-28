07f09ece0d40258f13307f2fc680de39d9c6d432
Nacer <41012118@parisnanterre.fr>
2021-10-28 13:43:02 +0200

    "Traductions multiples.QST4.1 addTranslation "

--- src/main/java/Dictionnaire/Dictionary.java
+++ src/main/java/Dictionnaire/Dictionary.java
@@ -1,16 +1,19 @@
 package Dictionnaire;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Dictionary {
 
     private String Name;
     private Map<String, String> translations;
+    private Map<String, List<String> >translationsMultp;
 
     public Dictionary( String name) {
         this.Name=name;
         this.translations=new HashMap<>();
+        this.translationsMultp=new HashMap<>();
     }
 
     public String getName() {
@@ -21,14 +24,19 @@ public class Dictionary {
         return this.translations.isEmpty();
     }
 
-    public void  addTranslation(String s1,String s2)
+    public void  addTranslation(String mot, List<String> traduction)
         {
-
+            this.translationsMultp.put(mot,traduction);
         }
 
-        public String getTranslation(String s)
+        public String getTranslation(String mot)
         {
          return  "against";
         }
+
+        public Map getMultipleTranslations(String mot){
+        return  this.translationsMultp;
+        }
+
 
 }
